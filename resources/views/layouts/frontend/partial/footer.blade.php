c201630ef3c51940693bee7b6f02274031d176b8
jannath <jannathsyeda7@gmail.com>
2021-02-15 00:35:40 +0600

    updated

--- resources/views/layouts/frontend/partial/footer.blade.php
+++ resources/views/layouts/frontend/partial/footer.blade.php
@@ -4,5 +4,5 @@
         <div class="container"style=" position:fixed; background-color:black;">
             <div class="row">
               <div class="col-md-12">
-   <span style="color:white;" >@ copy</span>
+   <span style="color:white;" >&copy</span>
               </div></div></div></section>
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome')->name('mainhome');
-// });
+Route::get('/', function () {
+    return view('welcome')->name('mainhome');
+});
 
 Auth::routes();
 
